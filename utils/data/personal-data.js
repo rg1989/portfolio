ec5e570adec7284e594b0d7e72a88ec1d91b9d0d
Roman <roman.g#payouts.com>
2025-02-18 11:01:52 +0200

    working order

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "Roman Grinevich",
   profile: "/profile.png",
-  designation: "FullStack Developer - Frontend lean",
+  designation: "FullStack Developer with a Frontend lean",
   description:
     "My name is Roman Grinevich. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript/TypeScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: "roman.grinevic@gmail.com",
