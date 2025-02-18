3248664c2875eedf9dc4e8c25fba95e423dad4f9
Roman Grinevich <roman.grinevic@gmail.com>
2025-02-19 01:09:11 +0200

    updated messaging

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -3,7 +3,7 @@ export const personalData = {
   profile: "/profile.png",
   designation: "FullStack Developer with a Frontend lean",
   description:
-    "My name is Roman Grinevich. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript/TypeScript. I am available for any kind of job opportunity that suits my skills and interests.",
+    "My name is Roman Grinevich. I am a dedicated and enthusiastic programmer with a strong focus on continuous self-learning. My expertise lies primarily in JavaScript and TypeScript, and I am passionate about exploring a wide range of web development stacks. I enjoy solving complex problems and strive to make the web more accessible and open. I am actively seeking job opportunities that align with my skills and interests. I enjoy teamwork, and always eager to learn and grow.",
   email: "roman.grinevic@gmail.com",
   phone: "+972542399894",
   address: "Tirza 3/6, Ramat-Gan, Israel",
