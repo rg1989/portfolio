1ab90f529e291f7c7e808073bc6bd297cbae49be
Roman <roman.g#payouts.com>
2025-02-21 02:58:45 +0200

    fixed phone num

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description:
     "My name is Roman Grinevich. I am a dedicated and enthusiastic programmer with a strong focus on continuous self-learning. My expertise lies primarily in JavaScript and TypeScript, and I am passionate about exploring a wide range of web development stacks. I enjoy solving complex problems and strive to make the web more accessible and open. I am actively seeking job opportunities that align with my skills and interests. I enjoy teamwork, and always eager to learn and grow.",
   email: "roman.grinevic@gmail.com",
-  phone: "+972 542399894",
+  phone: "+972-542399894",
   address: "Tirza 3/6, Ramat-Gan, Israel",
   github: "https://github.com/rg1989",
   facebook: "https://www.facebook.com/LeeeeeeeeeeroyJenkins",
